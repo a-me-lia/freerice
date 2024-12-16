fe780e843f2531868c8a5161463c0b2bfab57973
Matthew Guo <matthew@botbuilt.com>
2024-12-16 13:44:33 -0500

    added caffiene
    Signed-off-by: Matthew Guo <matthew@botbuilt.com>

--- sort/main.py
+++ sort/main.py
@@ -1,26 +1,6 @@
-import threading
-import time
-from typing import List
+import sys
+import subprocess
 
-def sleepsort(arr: List[int]) -> List[int]:
-    result = []
-    
-    def add_after_sleep(x):
-        time.sleep(x) 
-        result.append(x)
-
-    threads = []
-    for x in arr:
-        thread = threading.Thread(target=add_after_sleep, args=(x,))
-        threads.append(thread)
-        thread.start()
-    for thread in threads:
-        thread.join()
-    
-    return result
-
-if __name__ == "__main__":
-    test_array = [4, 1, 3000, 20, 7, 5, 6]
-    print("Original array:", test_array)
-    sorted_array = sleepsort(test_array)
-    print("Sorted array:", sorted_array)
+if 'darwin' in sys.platform:
+    print('Running \'caffeinate\' on MacOSX to prevent the system from sleeping')
+    subprocess.Popen('caffeinate')
